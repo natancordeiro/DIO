ec9e16bd22f161a52000c5ba3ba7853331961600
Natan Cordeiro <o.natan1907@gmail.com>
2022-11-06 16:17:49 -0300

    finalizei projeto "Smartphone" de POO

---
+++ Pottencial .NET Developer/_projetos/smartphone/Program.cs
@@ -0,0 +1,13 @@
+﻿using DesafioPOO.Models;
+
+Console.WriteLine("Smartphone Nokia: ");
+Smartphone nokia = new Nokia("9823532782", "Modelo 22", "111111", 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("Facebook");
+
+Console.WriteLine("\n");
+
+Console.WriteLine("Smartphone Iphone: ");
+Smartphone iphone = new Iphone("984357221", "14pro", "2222222", 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Whatsapp");
